2afab8701671c09a0b832cc43c8173ceaff3b49e
Demiauu <demianprivado1@gmail.com>
2024-11-25 20:55:09 -0300

    Se corrigio un sonido

--- configuracion/audio.py
+++ configuracion/audio.py
@@ -12,24 +12,28 @@ def reproducir_musica(ventana_actual, datos_juego):
         #utiliza el volumen designado para la musica y el sonido de los clicks 🌹
         pygame.mixer.Sound.set_volume(CLICK_SOUND,datos_juego['volumen_clicks'] / 100)
         pygame.mixer.Sound.set_volume(CLICK_ON_SOUND,datos_juego['volumen_clicks'] / 100)
+        pygame.mixer.Sound.set_volume(CLICK_SOUND_OUT,datos_juego['volumen_clicks'] / 100)
         pygame.mixer.music.set_volume(datos_juego['volumen_juego'] / 100)
     elif ventana_actual == "opciones":
         ruta_musica = "sonidos/musica.mp3"
         #utiliza el volumen designado para la musica y el sonido de los clicks 🌹
         pygame.mixer.Sound.set_volume(CLICK_SOUND,datos_juego['volumen_clicks'] / 100)
         pygame.mixer.Sound.set_volume(CLICK_ON_SOUND,datos_juego['volumen_clicks'] / 100)
+        pygame.mixer.Sound.set_volume(CLICK_SOUND_OUT,datos_juego['volumen_clicks'] / 100)
         pygame.mixer.music.set_volume(datos_juego['volumen_juego'] / 100)
     elif ventana_actual == "jugar":
         ruta_musica = "sonidos/Ignite - Zedd.WAV"
         #utiliza el volumen designado para la musica y el sonido de los clicks 🌹
         pygame.mixer.Sound.set_volume(CLICK_SOUND,datos_juego['volumen_clicks'] / 100)
         pygame.mixer.Sound.set_volume(CLICK_ON_SOUND,datos_juego['volumen_clicks'] / 100)
+        pygame.mixer.Sound.set_volume(CLICK_SOUND_OUT,datos_juego['volumen_clicks'] / 100)
         pygame.mixer.music.set_volume(datos_juego['volumen_juego'] / 100)
     elif ventana_actual == "ranking":
         ruta_musica = "sonidos/League of Legends - Warriors.WAV"
         #utiliza el volumen designado para la musica y el sonido de los clicks 🌹
         pygame.mixer.Sound.set_volume(CLICK_SOUND,datos_juego['volumen_clicks'] / 100)
         pygame.mixer.Sound.set_volume(CLICK_ON_SOUND,datos_juego['volumen_clicks'] / 100)
+        pygame.mixer.Sound.set_volume(CLICK_SOUND_OUT,datos_juego['volumen_clicks'] / 100)
         pygame.mixer.music.set_volume(datos_juego['volumen_juego'] / 100)
     else:
         return  #si no hay musica sale
